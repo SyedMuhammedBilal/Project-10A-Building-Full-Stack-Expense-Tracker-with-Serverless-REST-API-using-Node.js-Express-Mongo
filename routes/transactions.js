66b24d712b85dcfc509b6f99309b0bae89b8c04c
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-28 22:09:04 +0500

    Add files via upload

---
+++ routes/transactions.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { getTransactions,
+        addTransactions,
+        deleteTransactions } = require('../controllers/transactions.controller');
+
+router.route('/')
+    .get(getTransactions)
+    .post(addTransactions);
+
+router.route('/:id')
+    .delete(deleteTransactions)
+
+module.exports = router;
