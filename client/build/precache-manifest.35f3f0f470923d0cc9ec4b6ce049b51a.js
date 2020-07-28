d0aa996ae771bf7fe27ddacc0af91202f15e6971
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-28 22:06:26 +0500

    Add files via upload

---
+++ client/build/precache-manifest.35f3f0f470923d0cc9ec4b6ce049b51a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0ff144079c3665b379a6b213c4e799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48ddb1b71da915a1297d",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "ce1c6ca8db8e9403d1ca",
+    "url": "/static/js/2.270f4e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.270f4e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ddb1b71da915a1297d",
+    "url": "/static/js/main.3a1528f4.chunk.js"
+  },
+  {
+    "revision": "52c56a57f4ed61038532",
+    "url": "/static/js/runtime-main.6d9367bc.js"
+  }
+]);
